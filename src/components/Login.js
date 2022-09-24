2b60c73c84bb9a59f85cd39b39ff7b486c2fbdad
SiddharthM243 <siddharth243.sm@gmail.com>
2022-09-24 18:03:59 +0530

    Updated heading in Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
     <div>
 
       <form onSubmit={handleSubmit} className="login-form">
-        <h3>Login</h3>
+        <h3>Login Page</h3>
         <div>
           <label htmlFor="email">Email Id:</label>
           <input type="email" name="email" id="email" onChange={handleChange} value={email} />
